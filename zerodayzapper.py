eb7c88526766d171bb8efb28005ae9d125ad7998
Scott McAndrew <scottymcandrew@outlook.com>
2020-06-11 11:31:50 +0100

    Added HTML templates including a file upload form.

--- zerodayzapper.py
+++ zerodayzapper.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-  return 'Hello from Flask!'
+def homepage():
+  return render_template('index.html')
+
+
+@app.route('/success', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return render_template("success.html", name=f.filename)
+
 
 if __name__ == '__main__':
   app.run()
